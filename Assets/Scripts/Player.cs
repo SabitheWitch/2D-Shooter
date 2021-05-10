af9b67bcdf2acda1305217dc5b5909abc7e8df37
Sabrina <sabiwindsore@gmail.com>
2021-05-10 00:41:08 -0700

    Adding files for 2D shooter

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.5f;
+    void Start()
+    {
+        //take current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
+        //optimized version
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
