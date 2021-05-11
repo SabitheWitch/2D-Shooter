dd3ed2173f202a5a25424ea6b44c7e4d6f5cfbef
Sabrina <sabiwindsore@gmail.com>
2021-05-11 01:13:07 -0700

    Added boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,5 +22,31 @@ public class Player : MonoBehaviour
         //optimized version
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * speed * Time.deltaTime);
+
+        //if player position on y is greater than 0
+        //then y = 0
+        //if player position on y is less than -3.8f
+        //then y = -3.8f
+
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        } else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //if player position on x is greater than 11.24f
+        //then x = -11.24f
+        //if player on x is less than -11.24f
+        //then x = 11.24f
+
+        if (transform.position.x > 11.24f)
+        {
+            transform.position = new Vector3(-11.24f, transform.position.y, 0);
+        } else if (transform.position.x < -11.24f)
+        {
+            transform.position = new Vector3(11.24f, transform.position.y, 0);
+        }
     }
 }
