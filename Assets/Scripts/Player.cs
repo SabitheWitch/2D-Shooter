9b8e47be9be7751376550d2d29a9d8acb00a19a1
Sabrina <sabiwindsore@gmail.com>
2021-05-18 23:14:32 -0700

    Added laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,42 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10.5f;
+    [SerializeField]
+    private float _speed = 10.5f;
+
+    [SerializeField]
+    private GameObject _laserPreFab;
+
     void Start()
-    {
-        //take current position = new position (0,0,0)
+    {      
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        //if i hit space key
+        //spawn gameObject
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPreFab);
+        }
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        //transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
-
-        //optimized version
+        
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(direction * _speed * Time.deltaTime);
 
-        //if player position on y is greater than 0
-        //then y = 0
-        //if player position on y is less than -3.8f
-        //then y = -3.8f
+        //if(transform.position.y >= 0)
+        //{
+        //    transform.position = new Vector3(transform.position.x, 0, 0);
+        //} else if (transform.position.y <= -3.8f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        //}
 
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        } else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
-
-        //if player position on x is greater than 11.24f
-        //then x = -11.24f
-        //if player on x is less than -11.24f
-        //then x = 11.24f
+        // Line below works the same as the the if statement above that is commented.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
         if (transform.position.x > 11.24f)
         {
