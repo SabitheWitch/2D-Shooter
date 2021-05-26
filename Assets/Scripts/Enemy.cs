9cd68a7d7df74eb5f212a9a60f80b7bad521d8d4
Sabrina <sabiwindsore@gmail.com>
2021-05-25 21:23:14 -0700

    Added enemies and damage system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down 4m per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        /* 
+          if at bottom of screen
+          respawn at top with random x position 
+        */
+        if(transform.position.y < -4)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), 7, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /* if other is player
+         * Destroy enemy
+         * 
+         * if other is laser
+         * destroy laser
+         * destroy enemy */
+
+        if (other.tag == "Player")
+        {
+            //damage player
+            Player player = other.transform.GetComponent<Player>();
+            if(player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(gameObject);
+        }else if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
